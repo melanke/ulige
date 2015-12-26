017b73be472de8f682e1ccfbfe821d0fbc0f9c6a
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-12-25 23:13:17 -0200

    testando local not

---
+++ localnotification.js
@@ -0,0 +1,6 @@
+setInterval(function(){
+	self.registration.showNotification("Novo post", {  
+      body: "Na categoria \"Entretenimento\"",  
+      icon: "https://cdn.rawgit.com/melanke/ulige/master/imgs/logo.png"
+    });
+}, 5 * 60 * 1000);
