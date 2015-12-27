f03cba441d74fd0b2fe98d20eec741ae53dc205f
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-12-27 11:35:38 -0200

    ...

--- src/js/worker/localnotification.js
+++ src/js/worker/localnotification.js
@@ -4,11 +4,11 @@
 	    URL = require("../const/url.js"),
 	    simpleStorage = require("simpleStorage.js");
 
-	window.cb = function(data){
+	self.cb = function(data){
 	    console.log(data);
 	};
 
-	self.importScripts('https://melanke-test.blogspot.com.br/feeds/posts/default/?alt=json&max-results=10&callback=cb');
+	importScripts('https://melanke-test.blogspot.com.br/feeds/posts/default/?alt=json&max-results=10&callback=cb');
 
 	setInterval(function(){
 		self.registration.showNotification("Novo post", {  
