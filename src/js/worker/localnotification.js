9141777a437b1c0395004f650ab4b69909c85592
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-12-27 11:29:59 -0200

    ...

--- src/js/worker/localnotification.js
+++ src/js/worker/localnotification.js
@@ -4,11 +4,11 @@
 	    URL = require("../const/url.js"),
 	    simpleStorage = require("simpleStorage.js");
 
-	fetch("https://melanke-test.blogspot.com.br/feeds/posts/default/?alt=json&max-results=10", {
-	  mode: 'no-cors'
-	}).then(function(resp){
-		console.log(resp);
-	});
+	window.cb = function(data){
+	    console.log(data);
+	};
+
+	importScripts('https://melanke-test.blogspot.com.br/feeds/posts/default/?alt=json&max-results=10&callback=cb');
 
 	setInterval(function(){
 		self.registration.showNotification("Novo post", {  
