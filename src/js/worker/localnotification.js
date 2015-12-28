e7f5b4849c03764c3c97bd2ba4274fb089a00fca
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-12-28 12:05:28 -0200

    ...

--- src/js/worker/localnotification.js
+++ src/js/worker/localnotification.js
@@ -32,7 +32,7 @@
 				for (var j in post.category) {
 					var category = post.category[j];
 
-					if (categoriasQueSigo.indexOf(category.term)) {
+					if (categoriasQueSigo.indexOf(category.term) > -1) {
 						existeNova(post, category.term);
 						break; //uma notificação por vez
 					}
