f83cf81933994eb34ca2913918ac99dc9da74ad9
Gil Lopes Bueno <melanke.hc@gmail.com>
2015-12-28 13:18:56 -0200

    ...

--- src/js/worker/localnotification.js
+++ src/js/worker/localnotification.js
@@ -4,10 +4,13 @@
 		moment = require("moment");
 
 	var categoriasQueSigo,
-		horaUltimaNotificacao;
+		horaUltimaNotificacao,
+		urlNotificacao;
 
 	var init = function()
 	{
+		registerInteraction();
+
 		localforage.getItem("categoriasQueSigo", function(er, valueC) {
             categoriasQueSigo = valueC || [];
 
@@ -16,6 +19,9 @@
 
 				if (horaUltimaNotificacao) {
 					horaUltimaNotificacao = moment(horaUltimaNotificacao);
+				} else {
+					horaUltimaNotificacao = moment();
+					localforage.setItem("horaUltimaNotificacao", horaUltimaNotificacao.format());
 				}
 
 				initLoop();
@@ -53,6 +59,13 @@
 		horaUltimaNotificacao = moment();
 		localforage.setItem("horaUltimaNotificacao", horaUltimaNotificacao.format());
 
+		for (var j in post.link) {
+         	if (post.link[j].rel === "alternate") {
+                urlNotificacao = post.link[j].href;
+                break;
+            }
+        }
+
 		showNotification(post.title.$t, "Novo post em \""+categoria+"\"");
 	};
 
@@ -63,11 +76,44 @@
 	    });
 	};
 
+	var notificationClicked = function(event) {
+		event.notification.close();
+
+		if (!urlNotificacao) {
+			return;
+		}
+
+		//verifica se a pagina já está aberta para focar nela ou abrir uma nova
+		event.waitUntil(
+			clients.matchAll({  
+			  type: "window"  
+			}).then(function(clientList) {
+				
+			  for (var i = 0; i < clientList.length; i++) {  
+			    var client = clientList[i];  
+
+			    if (client.url.indexOf(urlNotificacao) > -1 && 'focus' in client)  
+			      return client.focus();  
+			    } 
+
+			    if (clients.openWindow) {
+			      return clients.openWindow(urlNotificacao);  
+			    }
+			})
+		);
+	};
+
 	var initLoop = function()
 	{
 		//a cada 5 minutos
-		//setInterval(checkNewPosts, 5 * 60 * 1000);
-		checkNewPosts();
+		setInterval(checkNewPosts, 5 * 60 * 1000);
+	};
+
+	var registerInteraction = function()
+	{
+		self.addEventListener('notificationclick', function(event) {  
+			notificationClicked(event);
+		});
 	};
 
 	init();
